cb5972c9031eee83642182c79679e86220561c3c
RebekaNagy <n.rebeka915@gmail.com>
2021-03-17 21:57:46 +0100

    #20 Added a responsive chip on the label editing screen.
    There is now a chip on the top of the screen. This chip represents the label we are editing. If we modify the TextInput or the ColorPicker  then the screen automatically update the chip.

--- components/LabelEditingScreen.tsx
+++ components/LabelEditingScreen.tsx
@@ -1,21 +1,83 @@
-import React, { ReactElement } from 'react';
-import { View } from 'react-native';
-import { TriangleColorPicker } from 'react-native-color-picker';
-import { Divider, Text, TextInput  } from 'react-native-paper';
+import { Guid } from 'guid-typescript';
+import React, { ReactElement, useState } from 'react';
+import { View, useWindowDimensions, } from 'react-native';
+import { TriangleColorPicker, fromHsv } from 'react-native-color-picker';
+import { Text, TextInput, Chip, Button  } from 'react-native-paper';
+import { Label } from '../interfaces/label';
 
 const LabelEditingScreen = (): ReactElement => {
 
+	const windowHeight = useWindowDimensions().height;
+
+	const [labelObject, changeObject] = useState<Label>({id: Guid.create(),text: 'Label', color: 'purple', photos: []});
+
+	const onTextChange = (value: string) => {
+		changeObject({...labelObject, text: value});
+	  }
+	  
+	  const onColorChange= (value: string) => {
+		changeObject({...labelObject, color: value});
+	  }
+
 	return (
-		<View style={{flex: 1, padding: 45, backgroundColor: '#cccccc' }}>
-			<Text style={{color: 'black', fontSize: 24}}>Label name:</Text>
-			<TextInput label="Label" mode='outlined' selectionColor='#ac5c5c'
-				theme={{ colors: { primary: '#ac5c5c', placeholder: '#ac5c5c', text: 'black',background : '#cccccc'}}} />
-			<Divider style={{backgroundColor: 'black'}}/>
-			<Text style={{color: 'black', fontSize: 24}}>Label color:</Text>
-			<TriangleColorPicker
-				oldColor='purple'
-				style={{flex: 1}}
-			/>
+		<View style={{
+				flex: 1, 
+				minHeight: Math.round(windowHeight) }}>
+			<View style={{ 
+					flex: 1, 
+					flexDirection: 'row', 
+					flexWrap: 'wrap', 
+					justifyContent: 'center', 
+					backgroundColor: '#cccccc', 
+					paddingTop: '5%' }}>
+				<Chip style={{ 
+						flexDirection: 'row', 
+						backgroundColor: labelObject.color}}
+					textStyle={{ color:'white',fontSize: 20, }}
+					mode='flat' 
+					children={labelObject.text} />
+			</View>
+			<View style={{ 
+				flex: 2, 
+				paddingHorizontal: 40, 
+				backgroundColor: '#cccccc'}}>
+				<Text style={{ color: '#ac5c5c', fontSize: 24 }}>
+					Label name:
+				</Text>
+				<TextInput  
+					mode='outlined' 
+					selectionColor='#ac5c5c' 
+					style={{fontSize: 20}}
+					theme={{ colors: { primary: '#ac5c5c', 
+									placeholder: '#ac5c5c', 
+									text: 'black', 
+									background: '#cccccc' } }}
+					onChangeText={(changedText => onTextChange(changedText))} />
+			</View>
+			<View style={{ 
+				flex: 10, 
+				padding: 40, 
+				backgroundColor: '#cccccc'}}>
+				<Text style={{ color: '#ac5c5c', fontSize: 24 }}>
+					Label color:
+				</Text>
+				<TriangleColorPicker
+					hideControls={true} 
+					style={{ flex: 1 }} 
+					oldColor={labelObject.color} 
+					onColorChange={(changedColor => onColorChange(fromHsv(changedColor)))}/>
+			</View>
+			<View style={{ 
+					flex: 3, 
+					flexDirection: 'row', 
+					flexWrap: 'wrap', 
+					justifyContent: 'flex-end', 
+					backgroundColor: '#cccccc', 
+					paddingRight: '5%'}}>
+				<Button icon='check-bold' mode='contained' color='#ac5c5c' labelStyle={{ color: '#cccccc'}}>
+					Confirm
+				</Button>
+			</View>
 		</View>
 	);
 };
