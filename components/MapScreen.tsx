7d1a788d6bb58b720d8640eb666415b732711a89
ZoltanTabi <62307692+ZoltanTabi@users.noreply.github.com>
2021-03-20 21:57:09 +0100

    Safety commit

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -1,10 +1,22 @@
-import React, { ReactElement } from 'react';
-import { View, Text } from 'react-native';
+import React, { ReactElement, useState } from 'react';
+import { View } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
+
+
 
 const MapScreen = (): ReactElement => {
+  const [region, setRegion] = useState<Region>( {latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 });
+
+  const onRegionChange = (newRegion: Region) => {
+    setRegion(newRegion);
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This will be the map screen.</Text>
+      <MapView
+        region={region}
+        onRegionChange={onRegionChange}
+      />
     </View>
   );
 };
--- ios/GeoGallery/AppDelegate.m
+++ ios/GeoGallery/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -43,6 +44,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"];
   return YES;
 }
 
