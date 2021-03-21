d34b120e1ef47d2ac54049535a9c173331854f35
ZoltanTabi <62307692+ZoltanTabi@users.noreply.github.com>
2021-03-21 15:36:04 +0100

    Added multiple photo action ( #11 )
    I added a new action for store more than one photos in state with one action

--- interfaces/photo.ts
+++ interfaces/photo.ts
@@ -23,6 +23,7 @@ export interface PhotoState {
 
 export const INIT_PHOTO_STATE = 'INIT_PHOTO_STATE';
 export const ADD_PHOTO = 'ADD_PHOTO';
+export const ADD_MULTIPLE_PHOTO = 'ADD_MULTIPLE_PHOTO';
 export const DELETE_PHOTO = 'DELETE_PHOTO';
 export const ADD_LABEL_TO_PHOTO = 'ADD_LABEL_TO_PHOTO';
 export const REMOVE_LABEL_FROM_PHOTO = 'REMOVE_LABEL_FROM_PHOTO';
@@ -36,6 +37,11 @@ interface InitPhotoStateAction {
 interface AddPhotoAction {
     type: typeof ADD_PHOTO
     payload: Photo
+}
+
+interface AddMultiplePhotoAction {
+    type: typeof ADD_MULTIPLE_PHOTO
+    payload: Photo[]
 }
 
 interface DeletePhotoAction {
@@ -70,6 +76,7 @@ interface RemoveLabelFromAllPhotoAction {
 
 export type PhotoActionTypes = InitPhotoStateAction
                              | AddPhotoAction
+                             | AddMultiplePhotoAction
                              | DeletePhotoAction
                              | AddLabelToPhotoAction
                              | RemoveLabelFromPhotoAction
