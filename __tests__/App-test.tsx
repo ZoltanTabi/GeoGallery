0f4c866493a3a59e84b912fc7a1435a33f36f3ef
ZoltanTabi <62307692+ZoltanTabi@users.noreply.github.com>
2021-03-26 20:35:23 +0100

    Fixed #32
    I added jest-transform-stub package and other jest configuration to project

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -28,6 +28,8 @@ jest.mock('@react-native-async-storage/async-storage', () => ({
 	})
 }));
 
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
 it('renders correctly', () => {
   jest.useFakeTimers();
   renderer.create(<App />);
