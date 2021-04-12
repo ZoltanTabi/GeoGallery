be85635842b1d9adc62a7655cb054dfe9e6344f2
RebekaNagy <n.rebeka915@gmail.com>
2021-04-12 17:10:43 +0200

    Image full screen, information, labels are functional.
    After tapping on a photo, it will appear in full screen.  The image now retains the aspect ratio of the width and height.
    The full screen has a FAB button on the right bottom.  From here, you can access image information, change the labels of the image, and delete the picture.
    The information contains the date, locations and labels.
    The label modification is a simple addition or removal from the label list.
    The delete feature are implemented but not perfect. After deleting the image, there is still a reference to the image, so we get an error.
    The above three features are all implemented with the Dialog component.

--- components/FullImageScreen.tsx
+++ components/FullImageScreen.tsx
@@ -1,24 +1,184 @@
-import { RouteProp, useRoute } from '@react-navigation/core';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/core';
 import React, { ReactElement } from 'react';
 import { View, Text, Image, useWindowDimensions } from 'react-native';
-import { useSelector } from 'react-redux';
+import { Badge, Button, Chip, Dialog, Divider, FAB, Modal, Paragraph, Portal, Provider } from 'react-native-paper';
+import { useDispatch, useSelector } from 'react-redux';
 import { Photo } from '../interfaces/photo';
 import { RootState } from '../storage';
+import { commonAddLabelToPhoto, commonDeletePhoto, commonRemoveLabelFromPhoto } from '../storage/actions/commonAction';
+import { removeLabelFromPhoto } from '../storage/actions/photoAction';
 
 const FullImageScreen = (): ReactElement => {
 
+	const dispatch = useDispatch();
+	const navigation = useNavigation();
     const windowWidth = useWindowDimensions().width;
 
     const route = useRoute<RouteProp<{ params: { id: string } }, 'params'>>();
 
 	const photoState = useSelector((state: RootState) => state.photoState);
+	const labelState = useSelector((state: RootState) => state.labelState);
 
 	const photoObject = photoState.photos.find((photo) => photo.id == route.params.id)as Photo;
 
+	const actLabels = labelState.labels.filter(label => photoObject.labels.includes(label.id));
+	const otherLabels = labelState.labels.filter(label => !(photoObject.labels.includes(label.id)));
+	const photoDate = photoObject.createDate != undefined ? photoObject.createDate : "No date information";	
+	const photoCountry = photoObject.country != undefined ? photoObject.country : "No country information";
+	const photoCity = photoObject.city != undefined ? photoObject.city : "No city information";
+
+	const [state, setState] = React.useState( false );
+	const onStateChange = (open: boolean) => setState( open );
+	const open = state;
+
+	const [visibleInfo, setVisibleInfo] = React.useState(false);
+	const showInfo = () => setVisibleInfo(true);
+  	const hideInfo = () => setVisibleInfo(false);
+
+	const [visibleDelete, setVisibleDelete] = React.useState(false);
+	const showDelete = () => setVisibleDelete(true);	
+	const hideDelete = () => setVisibleDelete(false);
+
+	const [visibleLabel, setVisibleLabel] = React.useState(false);
+	const showLabel = () => setVisibleLabel(true);	
+	const hideLabel = () => setVisibleLabel(false);
+
+	const onDelete = () => {
+		dispatch(commonDeletePhoto(photoObject));
+		hideDelete();
+    	navigation.goBack();
+  	}
+
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Image source={{uri: photoObject.imageUri}} 
-                    style={{ height: '50%', width: Math.round(windowWidth), margin: 5 }}/>
+		<View style={{ flex: 1}}>
+			<View style={{ flex: 18, justifyContent: 'center', alignItems: 'center' }}>
+				<Image source={{ uri: photoObject.imageUri }}
+					style={{ height: ((Math.round(windowWidth)/photoObject.width) * photoObject.height), width: Math.round(windowWidth)}} />
+			</View>			
+			<Provider>
+				<Portal>
+					<FAB.Group 
+					fabStyle={{backgroundColor: '#5c80ac'}}
+					color={'#cccccc'}
+					visible={true}
+					open={open}
+					icon={open ? 'close' : 'plus'}
+					actions={[
+						{
+							icon: 'information-outline',
+							color: '#cccccc',
+							style: {backgroundColor: '#5c80ac'},
+							onPress: showInfo,
+							small: false
+						},
+						{
+							icon: 'tag-text-outline',
+							color: '#cccccc',
+							style: {backgroundColor: '#5c80ac'},
+							onPress: showLabel,
+							small: false
+						},
+						{
+							icon: 'trash-can',
+							color: '#cccccc',
+							style: {backgroundColor: '#5c80ac'},
+							onPress: showDelete,
+							small: false
+						},
+					]}
+					onStateChange={(state) => onStateChange(state.open)}
+					onPress={() => {if (open) {}}}
+					/>
+					<Dialog visible={visibleInfo}
+						onDismiss={hideInfo}
+						style={{backgroundColor: '#cccccc'}}>
+						<Dialog.Title style={{color: '#5c80ac'}}>About photo</Dialog.Title>
+						<Dialog.Content>
+						<Paragraph style={{padding: 5, color: '#5c80ac'}}>Date: {photoDate}</Paragraph>
+						<Paragraph style={{padding: 5, color: '#5c80ac'}}>Country: {photoCountry}</Paragraph>						
+						<Paragraph style={{padding: 5, color: '#5c80ac'}}>City: {photoCity}</Paragraph>
+						<Paragraph style={{padding: 5, color: '#5c80ac'}}>Labels: {actLabels.length === 0 && "No labels"}</Paragraph>
+							<View style={{
+									flexDirection: 'row', 
+									flexWrap: 'wrap', 
+									justifyContent: 'flex-start', 
+									}}>
+								{
+								actLabels.map((item) => {
+								return (
+									<Chip
+										children={item.text}
+										mode="outlined" 
+										textStyle={{ color:'white',fontSize: 15 }}
+										style={{ margin: 4, backgroundColor: item.color }}
+										key={item.id}
+										/>
+									);
+								})}							
+      						</View>					
+						</Dialog.Content>
+					</Dialog>
+					<Dialog visible={visibleLabel}
+						dismissable={false}
+						style={{backgroundColor: '#cccccc'}}>
+						<Dialog.Title style={{color: '#5c80ac'}}>Modify labels</Dialog.Title>
+						<Dialog.Content>							
+							<Paragraph style={{padding: 5, color: '#5c80ac'}}>Labels of the photo:</Paragraph>
+							<View style={{
+									flexDirection: 'row', 
+									flexWrap: 'wrap', 
+									justifyContent: 'center', 
+									}}>
+								{
+								actLabels.map((item) => {
+								return (
+									<Chip
+										children={item.text}
+										mode="outlined" 
+										textStyle={{ color:'white',fontSize: 15 }}
+										style={{ margin: 4, backgroundColor: item.color }}
+										key={item.id}
+										onClose={() => dispatch(commonRemoveLabelFromPhoto(photoObject.id, item.id))}
+										/>
+									);
+								})}							
+      						</View>					
+							<Paragraph style={{padding: 5, color: '#5c80ac'}}>Other labels:</Paragraph>
+							<View style={{
+									flexDirection: 'row', 
+									flexWrap: 'wrap', 
+									justifyContent: 'center', 
+									}}>
+								{
+								otherLabels.map((item) => {
+								return (
+									<Chip
+										children={item.text}
+										mode="outlined" 
+										textStyle={{ color:'white',fontSize: 15 }}
+										style={{ margin: 4, backgroundColor: item.color }}
+										key={item.id}
+										onPress={() => dispatch(commonAddLabelToPhoto(photoObject.id, item.id))}
+										/>
+									);
+								})}							
+      						</View>
+						</Dialog.Content>
+						<Dialog.Actions>
+							<Button color='#5c80ac' onPress={hideLabel}>Confirm</Button>
+						</Dialog.Actions>
+					</Dialog>
+					<Dialog visible={visibleDelete}
+						dismissable={false}
+						style={{backgroundColor: '#cccccc'}}>
+						<Dialog.Title style={{color: '#5c80ac'}}>Delete this photo?</Dialog.Title>
+						<Dialog.Actions>
+							<Button color='#5c80ac' onPress={hideDelete}>Cancel</Button>
+							<Button color='#ac5c5c' onPress={onDelete}>Delete</Button>
+						</Dialog.Actions>
+					</Dialog>
+				</Portal>
+			</Provider>
 		</View>
 	);
 };
